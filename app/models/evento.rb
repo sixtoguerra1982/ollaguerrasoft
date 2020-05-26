89f7929a24472912011fec36a33a60dc45499619
Sixto Guerra <sixto.guerra1982@gmail.com>
2020-05-25 22:28:20 -0400

    evento pertenece to user)

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -2,4 +2,12 @@ class Evento < ApplicationRecord
   belongs_to :comuna
   belongs_to :user
   validates :descripcion, presence: true
+
+  def pertenece_to_user(user)
+  	if user.present? && self.user_id == user.id
+  		true
+  	else
+  		false
+  	end
+  end
 end
