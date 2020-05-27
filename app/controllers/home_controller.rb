8cb62c86296839128aeb1122ab76b0e53047f254
Sixto Guerra <sixto.guerra1982@gmail.com>
2020-05-27 00:01:54 -0400

    new version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   		@eventos = Evento.all.where(comuna_id: params[:search].to_i)
       @cantidad = @eventos.count
   	else
-  		@eventos = Evento.all
+  		@eventos = Evento.all.order(updated_at: :desc)
   	end
   end
 
--- db/seeds/03_usuarios.rb
+++ db/seeds/03_usuarios.rb
@@ -1,8 +1,10 @@
 puts '*** Cargando usuarios...'
+User.all.destroy_all
 13.times do |i|
  i+=1
  u = User.find_by(id: i)
  if  u.nil?
- 	User.create(email: "correo#{i}@corre.cl", password: '123123')
+ 	User.create(email: "correo#{i}@correo.cl", password: '123123')
  end
 end
+User.create(email: "admin@admin.cl", password: 'admin123')
